29abd1c84878fe1729f335d2a1a1e49fa26972cf
mikesmall <michael.l.a.small@gmail.com>
2017-10-19 17:05:53 -0400

    'It is time for tea' added as an <h3>. Works.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class Hello extends Component {
     return (
       <div>
         <h1>Hello World!</h1>
-        <p>Mavis is awesome.</p>
+        <h3>It is time for tea.</h3>
       </div>
     )
   }
