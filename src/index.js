e64d976555f242a5686300cea1319c3e2b99e027
mikesmall <michael.l.a.small@gmail.com>
2017-12-07 17:44:09 -0500

    Adds 'favourites' array to 'cat' object

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ import Hello from './App.js';
 
 var cat = {
   catName: "Mavis",
-  catAge: 13
+  catAge: 13,
+  favourites: [
+    "tuna",
+    "chips",
+    "coconut oil"
+  ]
 }
 
 ReactDOM.render(
   <Hello
     name={cat.catName}
     age={cat.catAge}
+    treats={cat.favourites}
   />,
   document.getElementById('root')
 )
