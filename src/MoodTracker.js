bf48164fc66700ac9004dcc7fdbe76c9b6c71738
mikesmall <michael.l.a.small@gmail.com>
2017-12-08 13:20:04 -0500

    MoodTracker component established in MoodTracker.js

---
+++ src/MoodTracker.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class MoodTracker extends Component {
+  render () {
+    return (
+      <div>
+        <p>MoodTracker!</p>
+      </div>
+    )
+  }
+}
+
+export default MoodTracker
