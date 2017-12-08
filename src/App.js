3bdd16a6bfa6a04d65e082de8288a6ab273cf84c
mikesmall <michael.l.a.small@gmail.com>
2017-12-08 13:51:10 -0500

    Creates a trigger event to change the moodPoints counter

--- src/App.js
+++ src/App.js
@@ -2,22 +2,31 @@
 import React, {Component} from 'react';
 
 class Hello extends Component {
+
   // what should happen when the component is first created
   constructor (props) {
     super()
     this.state = {
       moodPoints: 1 // initializes this.state.moodPoints to be 1
     }
+
   }
-  // what should the component render?
+
+  // a trigger event to change the mood counter:
+  increaseMood(e) {
+    this.setState({
+      moodPoints: this.state.moodPoints +1
+    });
+  }
+
+  // render/return UI:
   render () {
-    // make sure to return some UI
     return (
       <div>
         <h1>Hello {this.props.name}!</h1>
         <h3>It is time for treats.</h3>
         <p>Also, you are {this.props.age} years old.</p>
-        <p>How about some {this.props.treats[0]}?</p>
+        <p>Want some {this.props.treats[0]}?</p>
         <p>On a scale of 1 to 10, you are this happy: {this.state.moodPoints}</p>
       </div>
     )
