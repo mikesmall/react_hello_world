e1d6ce059e85149f53ebbf9d5392e13793375709
mikesmall <michael.l.a.small@gmail.com>
2017-12-08 13:59:00 -0500

    Adds comments to App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 // bring in React and Component from React
 import React, {Component} from 'react';
 
+// define the Hello component
 class Hello extends Component {
 
   // what should happen when the component is first created
   constructor (props) {
+    // make call to parent class' (Component) constructor
     super()
+    // define an initial state
     this.state = {
       moodPoints: 1 // initializes this.state.moodPoints to be 1
     }
 
   }
 
-  // a trigger event to change the mood counter:
+  // a trigger event to change the mood counter (increase by 1):
   increaseMood(e) {
     this.setState({
       moodPoints: this.state.moodPoints +1
